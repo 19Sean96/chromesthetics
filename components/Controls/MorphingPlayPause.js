8f71021dd352821fca99e085b5c17eff407c0533
Sean Alexander <me@seananthony.io>
2021-10-17 13:27:28 -0700

    updated player control classes and layout

--- components/Controls/MorphingPlayPause.js
+++ components/Controls/MorphingPlayPause.js
@@ -23,40 +23,39 @@ const MorphingPlayPause = ({ isPlaying, onClick }) => {
 	}, [isPlaying]);
 
 	return (
-		<div className="player--play player--controls--button__wrapper">
-			<button
-				className="player--play__button player--controls--button"
-				onClick={onClick}
+		<button
+			className="player--play__button btn btn--play"
+			onClick={onClick}
+			id="play"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 31.5 31.5"
+				style={{
+					transform: !isPlaying
+						? "translate(-42.5%, -40%)"
+						: "translate(-50%, -50%)",
+				}}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 31.5 31.5"
-					style={{
-						transform: !isPlaying
-							? "translate(-42.5%, -40%)"
-							: "translate(-50%, -50%)",
-					}}
-				>
-					<path
-						id="Icon_awesome-pause"
-						dataname="Icon awesome-pause"
-						d="M10.125,33.68H3.375A3.376,3.376,0,0,1,0,30.3V5.555A3.376,3.376,0,0,1,3.375,2.18h6.75A3.376,3.376,0,0,1,13.5,5.555V30.3A3.376,3.376,0,0,1,10.125,33.68ZM31.5,30.3V5.555A3.376,3.376,0,0,0,28.125,2.18h-6.75A3.376,3.376,0,0,0,18,5.555V30.3a3.376,3.376,0,0,0,3.375,3.375h6.75A3.376,3.376,0,0,0,31.5,30.3Z"
-						transform={"translate(0 -2.18)"}
-						fill="#f5f5f5"
-						ref={pausePath}
-					/>
-					<path
-						id="Icon_awesome-play"
-						dataname="Icon awesome-play"
-						d="M29.841,13.209,5.091.406C3.08-.633,0,.376,0,2.947v25.6c0,2.307,2.862,3.7,5.091,2.541l24.751-12.8a2.775,2.775,0,0,0,0-5.082Z"
-						transform="translate(0 -0.002)"
-						fill="#fff"
-						ref={playPath}
-						style={{ visibility: "hidden" }}
-					/>
-				</svg>
-			</button>
-		</div>
+				<path
+					id="Icon_awesome-pause"
+					dataname="Icon awesome-pause"
+					d="M10.125,33.68H3.375A3.376,3.376,0,0,1,0,30.3V5.555A3.376,3.376,0,0,1,3.375,2.18h6.75A3.376,3.376,0,0,1,13.5,5.555V30.3A3.376,3.376,0,0,1,10.125,33.68ZM31.5,30.3V5.555A3.376,3.376,0,0,0,28.125,2.18h-6.75A3.376,3.376,0,0,0,18,5.555V30.3a3.376,3.376,0,0,0,3.375,3.375h6.75A3.376,3.376,0,0,0,31.5,30.3Z"
+					transform={"translate(0 -2.18)"}
+					fill="#f5f5f5"
+					ref={pausePath}
+				/>
+				<path
+					id="Icon_awesome-play"
+					dataname="Icon awesome-play"
+					d="M29.841,13.209,5.091.406C3.08-.633,0,.376,0,2.947v25.6c0,2.307,2.862,3.7,5.091,2.541l24.751-12.8a2.775,2.775,0,0,0,0-5.082Z"
+					transform="translate(0 -0.002)"
+					fill="#fff"
+					ref={playPath}
+					style={{ visibility: "hidden" }}
+				/>
+			</svg>
+		</button>
 	);
 };
 
--- components/Controls/index.js
+++ components/Controls/index.js
@@ -24,45 +24,113 @@ const Controls = ({
 	return (
 		<div className="dashboard">
 			<section className="player">
-				<MorphingPlayPause
-					isPlaying={isPlaying}
-					onClick={() => {
-						player
-							.togglePlay()
-							.then(
-								() =>
-									!isPlaying &&
-									cancelAnimationFrame(timeRequestRef.current)
-							);
-					}}
-				/>
 				<div className="player--controls">
 					<div className="player--controls--buttons">
-						<div className="player--skip-back player--controls--button__wrapper">
-							<button
-								className="player--skip-back__button player--controls--button"
-								onClick={() => player.previousTrack()}
-							>
-								<FontAwesomeIcon icon={faFastBackward} />
-							</button>
-						</div>
-						<div className="player--back-30 player--controls--button__wrapper">
-							<button
-								onClick={() => {
-									let newTime;
-									if (time.current <= 30000) {
-										newTime = 0;
-									} else newTime = time.current - 30000;
-									player.seek(newTime);
-								}}
-								className="player--back-30__button player--controls--button"
-							>
-								<span className="thirty">30</span>
-								<FontAwesomeIcon icon={faUndoAlt} />
-							</button>
-						</div>
+						<button
+							className="btn"
+							id="prev"
+							onClick={(e) => player.previousTrack()}
+						>
+							<FontAwesomeIcon icon={faFastBackward} />
+						</button>
+						<button
+							className="btn"
+							id="backward30"
+							onClick={(e) => {
+								if (time.current - 30 * 1000 < 0) {
+									player.seek(0);
+								} else {
+									player.seek(time.current - 30 * 1000);
+								}
+							}}
+						>
+							<FontAwesomeIcon icon={faUndoAlt} />
+						</button>
+						<MorphingPlayPause
+							isPlaying={isPlaying}
+							onClick={() => {
+								player
+									.togglePlay()
+									.then(
+										() =>
+											!isPlaying &&
+											cancelAnimationFrame(
+												timeRequestRef.current
+											)
+									);
+							}}
+						/>
+						<button
+							className="btn"
+							id="forward30"
+							onClick={(e) => {
+								if (time.current + 30 * 1000 > time.total) {
+									player.nextTrack();
+								} else {
+									player.seek(time.current + 30 * 1000);
+								}
+							}}
+						>
+							<FontAwesomeIcon icon={faRedoAlt} />
+						</button>
+						<button
+							className="btn"
+							id="next"
+							onClick={() => player.nextTrack()}
+						>
+							<FontAwesomeIcon icon={faFastForward} />
+						</button>
 					</div>
 				</div>
+				<div className="player--progressbar">
+					<input
+						type="range"
+						name="progress"
+						id="progress"
+						className="player--progressbar__input"
+						min="0"
+						value={time.current}
+						max={time.total}
+						onChange={(e) => player.seek(e.target.value)}
+					/>
+				</div>
+				{track && (
+					<div className="player--infobox">
+						<div className="player--infobox__covers">
+							{track?.previous_tracks && track.previous_tracks.length > 0 && (
+								<img
+									src={
+										track.previous_tracks[0].album.images[2]
+											.url
+									}
+									alt={track.previous_tracks[0].album.name}
+									className="cover-previous"
+									onClick={() => player.previousTrack()}
+								/>
+							)}
+
+							<img
+								src={track.current_track.album.images[2].url}
+								alt={track.current_track.album.name}
+								className="cover-current"
+							/>
+							<img
+								src={track.next_tracks[0].album.images[2].url}
+								alt={track.next_tracks[0].album.name}
+								className="cover-next"
+								onClick={() => player.nextTrack()}
+							/>
+						</div>
+						<div className="player--infobox__track-info">
+							<h1 className="artist">
+								{track.current_track.artists
+									.map((el) => el.name)
+									.join(", ")}
+							</h1>
+							<h2 className="song">{track.current_track.name}</h2>
+						</div>
+					</div>
+				)}
 			</section>
 		</div>
 	);
